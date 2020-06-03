cc70955537bc15c06cad5de009919461bfcfacfc
William Turatti <wturatti@gmail.com>
2020-06-03 15:24:20 -0300

    Build folder

---
+++ build/precache-manifest.fa6a678285e295f926807b93ef16f930.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0e1edd1d7d5238dcdb113d46cc19df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee848ecab2547ee1e4f9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2cbdcc5e2bfca34e5f6e",
+    "url": "/static/js/2.caa675d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.caa675d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee848ecab2547ee1e4f9",
+    "url": "/static/js/main.dbaccb04.chunk.js"
+  },
+  {
+    "revision": "4e2eaee1d9b4aa0eb932",
+    "url": "/static/js/runtime-main.54ae76d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
